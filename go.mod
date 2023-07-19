b91289ea80eb6b855c73f760a309eb8f1836d9e4
Ammar Bandukwala <ammar@ammar.io>
2023-07-19 11:12:15 -0500

    Use cobra

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.14
 
 require (
 	github.com/aclements/go-moremath v0.0.0-20190830160640-d16893ddf098
-	github.com/spf13/pflag v1.0.5
-	go.coder.com/cli v0.4.0
-	go.coder.com/flog v0.0.0-20190906214207-47dd47ea0512
+	github.com/coder/flog v1.1.0
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/spf13/cobra v1.7.0
 	golang.org/x/sys v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,50 +10,46 @@ import (
 	"time"
 
 	"github.com/aclements/go-moremath/stats"
-	"github.com/spf13/pflag"
-	"go.coder.com/cli"
-	"go.coder.com/flog"
+	"github.com/coder/flog"
+	"github.com/spf13/cobra"
 )
 
-type rootCmd struct {
+var rootCmd = &cobra.Command{
+	Use:   "timer [flags] [test command ...]",
+	Short: "Measure the performance of command execution",
+	Run:   executeCommand,
+}
+
+var (
 	iterations  int
 	parallelism int
 	quiet       bool
+)
+
+func init() {
+	rootCmd.PersistentFlags().IntVarP(&iterations, "iterations", "n", 0, "number of iterations")
+	rootCmd.PersistentFlags().IntVarP(&parallelism, "parallelism", "p", 1, "number of concurrent workers")
+	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "don't show command output")
 }
 
-func (r *rootCmd) RegisterFlags(fl *pflag.FlagSet) {
-	fl.SetInterspersed(false)
-	fl.IntVarP(&r.iterations, "iterations", "n", 0, "number of iterations")
-	fl.IntVarP(&r.parallelism, "parallelism", "p", 1, "number of concurrent workers")
-	fl.BoolVarP(&r.quiet, "quiet", "q", false, "don't show command output")
-}
-
-func (r *rootCmd) Spec() cli.CommandSpec {
-	return cli.CommandSpec{
-		Name:  "timer",
-		Usage: "[flags] [test command ...]",
-		Desc:  "Measure the performance of command execution",
-	}
-}
-
-func (r *rootCmd) Run(fl *pflag.FlagSet) {
-	if r.iterations == 0 {
-		flog.Fatal("iterations (-n) must be provided")
+func executeCommand(cmd *cobra.Command, args []string) {
+	if iterations == 0 {
+		flog.Fatalf("iterations (-n) must be provided")
 	}
 
-	command := fl.Args()
+	command := args
 	if len(command) == 0 {
-		flog.Fatal("command must provided")
+		flog.Fatalf("command must provided")
 	}
 
 	var (
 		commands   = make(chan *exec.Cmd)
 		tooksMu    sync.Mutex
-		tooks      = make([]float64, 0, r.iterations)
+		tooks      = make([]float64, 0, iterations)
 		totalStart = time.Now()
 	)
 	var commandWaitGroup sync.WaitGroup
-	for i := 0; i < r.parallelism; i++ {
+	for i := 0; i < parallelism; i++ {
 		commandWaitGroup.Add(1)
 		go func() {
 			defer commandWaitGroup.Done()
@@ -67,7 +63,7 @@ func (r *rootCmd) Run(fl *pflag.FlagSet) {
 				tooksMu.Unlock()
 
 				if err != nil {
-					flog.Fatal("command execution failed: %+v", err)
+					flog.Fatalf("command execution failed: %+v", err)
 				}
 			}
 		}()
@@ -75,9 +71,9 @@ func (r *rootCmd) Run(fl *pflag.FlagSet) {
 
 	go func() {
 		defer close(commands)
-		for i := 0; i < r.iterations; i++ {
+		for i := 0; i < iterations; i++ {
 			cmd := exec.Command(command[0], command[1:]...)
-			if !r.quiet {
+			if !quiet {
 				cmd.Stdout = os.Stdout
 				cmd.Stderr = os.Stderr
 			}
@@ -94,13 +90,13 @@ func (r *rootCmd) Run(fl *pflag.FlagSet) {
 	wr := tabwriter.NewWriter(os.Stdout, 6, 4, 4, ' ', 0)
 	defer wr.Flush()
 	// This newline prefix helps when the output is mangled.
-	if !r.quiet {
+	if !quiet {
 		fmt.Fprintf(wr, "\n")
 	}
 	fmt.Fprintf(wr, "--- timer config\n")
 	fmt.Fprintf(wr, "command\t%s\n", strings.Join(command, " "))
-	fmt.Fprintf(wr, "iterations\t%v\n", r.iterations)
-	fmt.Fprintf(wr, "parallelism\t%v\n", r.parallelism)
+	fmt.Fprintf(wr, "iterations\t%v\n", iterations)
+	fmt.Fprintf(wr, "parallelism\t%v\n", parallelism)
 	fmt.Fprintf(wr, "--- percentiles\n")
 	fmt.Fprintf(wr, "0\t(fastest)\t%.3fs\n", sample.Quantile(0))
 	fmt.Fprintf(wr, "25\t(1st quantile)\t%.3fs\n", sample.Quantile(0.25))
@@ -111,9 +107,11 @@ func (r *rootCmd) Run(fl *pflag.FlagSet) {
 	fmt.Fprintf(wr, "total\t%.3fs\n", totalTook.Seconds())
 	fmt.Fprintf(wr, "mean\t%.3fs\n", sample.Mean())
 	fmt.Fprintf(wr, "stddev\t%.3fs\n", sample.StdDev())
-
 }
 
 func main() {
-	cli.Run(&rootCmd{}, os.Args[1:], "")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
